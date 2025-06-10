1ea3980b8dd157904ac2a3937d7b57afebca6fdb
Jesse Hernandez <jesseovr@gmail.com>
2025-06-10 15:17:31 -0500

    gtag and vercel analytics added

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,8 @@ import { Geist } from "next/font/google"
 import "./globals.css"
 import Navbar from "./components/Navbar"
 import Footer from "./components/footer"
+import Script from "next/script"
+import { Analytics } from "@vercel/analytics/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -17,12 +19,14 @@ const geistSans = Geist({
 
 export const metadata: Metadata = {
   title: "Digital Canvas | San Antonio's Creative Tech Community",
-  description: "Connect with fellow creators, share your projects, and dive into the dynamic intersection of art, tech, and music in San Antonio.",
+  description:
+    "Connect with fellow creators, share your projects, and dive into the dynamic intersection of art, tech, and music in San Antonio.",
   keywords: "creative tech, San Antonio, coworking, community, design, development, photography, videography",
   authors: [{ name: "Digital Canvas Team" }],
   openGraph: {
     title: "Digital Canvas | San Antonio's Creative Tech Community",
-    description: "Connect with fellow creators, share your projects, and dive into the dynamic intersection of art, tech, and music in San Antonio.",
+    description:
+      "Connect with fellow creators, share your projects, and dive into the dynamic intersection of art, tech, and music in San Antonio.",
     url: "https://digitalcanvas.community",
     siteName: "Digital Canvas",
     images: [
@@ -39,7 +43,8 @@ export const metadata: Metadata = {
   twitter: {
     card: "summary_large_image",
     title: "Digital Canvas | San Antonio's Creative Tech Community",
-    description: "Connect with fellow creators, share your projects, and dive into the dynamic intersection of art, tech, and music in San Antonio.",
+    description:
+      "Connect with fellow creators, share your projects, and dive into the dynamic intersection of art, tech, and music in San Antonio.",
     images: ["https://digitalcanvas.community/opengraph-image.png"],
   },
 }
@@ -54,11 +59,23 @@ export default function RootLayout({
       <body
         className={`${geistSans.className} min-h-screen overflow-x-hidden scroll-auto bg-gray-50 antialiased selection:bg-sky-100 selection:text-sky-600`}
       >
+        {/* Google Analytics */}
+        <Script src="https://www.googletagmanager.com/gtag/js?id=G-NY5R12BN23" strategy="afterInteractive" />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-NY5R12BN23');
+          `}
+        </Script>
+
         <Navbar />
         {children}
         <Footer />
+
+        <Analytics />
       </body>
     </html>
   )
 }
-
