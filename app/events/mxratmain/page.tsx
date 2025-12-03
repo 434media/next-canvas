4ea332827548eb1ee61cfa5a1f4d355d8e5db35c
Jesse Hernandez <jesseovr@gmail.com>
2025-12-02 19:01:17 -0600

    og review

--- app/events/mxratmain/page.tsx
+++ app/events/mxratmain/page.tsx
@@ -16,9 +16,6 @@ export const metadata: Metadata = {
   authors: [{ name: "434 Media" }],
   creator: "Digital Canvas",
   publisher: "434 Media",
-  other: {
-    'image': 'https://digitalcanvas.community/events/mxratmain/opengraph-image.png',
-  },
   openGraph: {
     title: "MXR @MAIN | Holiday Party",
     description:
@@ -26,10 +23,10 @@ export const metadata: Metadata = {
     type: "website",
     locale: "en_US",
     siteName: "Digital Canvas",
-    url: "https://digitalcanvas.community/events/mxratmain",
+    url: "/events/mxratmain",
     images: [
       {
-        url: 'https://digitalcanvas.community/events/mxratmain/opengraph-image.png',
+        url: '/events/mxratmain/opengraph-image.png',
         width: 1200,
         height: 630,
         alt: 'MXR @MAIN | Holiday Party by 434 Media',
@@ -42,7 +39,7 @@ export const metadata: Metadata = {
     title: "MXR @MAIN | Holiday Party by 434 Media",
     description:
       "A holiday party for molecules & musicians, scientists & screwups, techies & teachers, founders & funders, gamers & grunts.",
-    images: ['https://digitalcanvas.community/events/mxratmain/opengraph-image.png'],
+    images: ['/events/mxratmain/opengraph-image.png'],
   },
   robots: {
     index: true,
@@ -56,7 +53,7 @@ export const metadata: Metadata = {
     },
   },
   alternates: {
-    canonical: 'https://digitalcanvas.community/events/mxratmain',
+    canonical: '/events/mxratmain',
   },
 }
 
